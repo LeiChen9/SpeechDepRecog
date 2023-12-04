1bd64eebdeacf7bafb452b0dcfbbc0456a4d298e
Chen,Lei (BI X) BIIT-CN-S <lei_2.chen@boehringer-ingelheim.com>
2023-12-04 17:38:49 +0800

    init model

--- Yoda/models/trans.py
+++ Yoda/models/trans.py
@@ -60,18 +60,33 @@ class DepressionClassifier(nn.Module):
         self.dropout_linear = nn.Dropout(p=0)
         self.out_softmax = nn.Softmax(dim=1)
 
-    def forward(self,x):
+    def forward(self,
+                pos_deep_embed: torch.Tensor,
+                pos_mel_feat: torch.Tensor, 
+                neg_deep_embed: torch.Tensor, 
+                neg_mel_feat: torch.Tensor, 
+                neutral_deep_embed: torch.Tensor, 
+                neutral_mel_feat: torch.Tensor):
         # conv embedding
-        conv_embedding = self.conv2Dblock(x) #(b,channel,freq,time)
-        conv_embedding = torch.flatten(conv_embedding, start_dim=1) # do not flatten batch dimension
+        pos_conv_embedding = self.conv2Dblock(pos_mel_feat) #(b,channel,freq,time)
+        neg_conv_embedding = self.conv2Dblock(neg_mel_feat)
+        neutral_conv_embedding = self.conv2Dblock(neg_mel_feat)
+        # conv_embedding = torch.flatten(conv_embedding, start_dim=1) # do not flatten batch dimension
+        pos_embed = torch.flatten(pos_conv_embedding, start_dim=1)
+        neg_embed = torch.flatten(neg_conv_embedding, start_dim=1)
+        neutral_embed = torch.flatten(neutral_conv_embedding, start_dim=1)
         # transformer embedding
-        x_reduced = self.transf_maxpool(x)
-        x_reduced = torch.squeeze(x_reduced,1)
-        x_reduced = x_reduced.permute(2,0,1) # requires shape = (time,batch,embedding)
-        transf_out = self.transf_encoder(x_reduced)
-        transf_embedding = torch.mean(transf_out, dim=0)
+        # x_reduced = self.transf_maxpool(x)
+        # x_reduced = torch.squeeze(x_reduced,1)
+        # x_reduced = x_reduced.permute(2,0,1) # requires shape = (time,batch,embedding)
+        pos_transf_out = self.transf_encoder(pos_deep_embed)
+        pos_transf_embedding = torch.mean(pos_transf_out, dim=0)
+        neg_transf_out = self.transf_encoder(neg_deep_embed)
+        neg_transf_embedding = torch.mean(neg_transf_out, dim=0)
+        neutral_transf_out = self.transf_encoder(neutral_deep_embed)
+        neutral_transf_embedding = torch.mean(neutral_transf_out, dim=0)
         # concatenate
-        complete_embedding = torch.cat([conv_embedding, transf_embedding], dim=1) 
+        complete_embedding = torch.cat([pos_embed, neg_embed, neutral_embed,pos_transf_embedding,neg_transf_embedding,neutral_transf_embedding], dim=1) 
         # final Linear
         output_logits = self.out_linear(complete_embedding)
         output_logits = self.dropout_linear(output_logits)
