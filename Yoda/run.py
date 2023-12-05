9795551cd99ee8f97fbf7780e072550908d71e7e
Chen,Lei (BI X) BIIT-CN-S <lei_2.chen@boehringer-ingelheim.com>
2023-12-05 09:47:55 +0800

    xxx

--- Yoda/run.py
+++ Yoda/run.py
@@ -2,20 +2,33 @@ import numpy as np
 import pandas as pd
 import os
 import pdb
+import argparse
+import torch
+from dataloader import SDRDataLoader
+from models.trans import DepressionClassifier
 from utils import *
 import yaml
 
 if __name__ == '__main__':
-    # load data config
-    with open("./configs/data_config.yaml", "r") as f:
-        data_config = yaml.safe_load(f)
-    # load and parse data
-    for key, value in data_config.items():
-        with open(value['label'], 'r') as f:
-            score = f.read()
-            score = float(score)
-            if score >= 53:
-                label = 1
-            else:
-                label = 0
-            pdb.set_trace()
+    parser =  argparse.ArgumentParser(
+        description="FunASR Common Training Parser",
+    )
+    parser.add_argument("--seed", type=int, default=0, help="Random seed")
+    parser.add_argument("--data_config", type=str, default="./configs/data_config.yaml", help="Data config file")
+    parser.add_argument("--batch_size", type=int, default=8, help="Train batch size")
+    parser.add_argument("--train_dtype", default="float32", choices=["float16", "float32", "float64"], help="Data type for training.")
+    args = parser.parse_args()
+
+    # set random seed
+    set_all_random_seed(args.seed)
+
+    # get data
+    data_loader = SDRDataLoader(data_config_file=args.data_config, batch_size=args.batch_size)
+
+    # get model
+    model = SDRDataLoader()
+    model = model.to(
+        dtype=getattr(torch, args.train_dtype),
+        device="cuda" if args.ngpu > 0 else "cpu",
+    )
+    
--- Yoda/utils.py
+++ Yoda/utils.py
@@ -1,4 +1,7 @@
 import re 
+import random 
+import torch
+import numpy as np
 
 def check_string(re_exp, str):
     res = re.search(re_exp, str)
@@ -6,3 +9,8 @@ def check_string(re_exp, str):
         return True 
     else:
         return False
+    
+def set_all_random_seed(seed: int):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.random.manual_seed(seed)
